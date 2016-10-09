f0b7998beca2769058111ac5b9c49e90a6d3438a
ipek-kk <ipekkarakurt@yandex.com>
2016-10-09 23:26:34 +0300

    Port change for Bluemix deployment

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ Created on Oct 8, 2016
 @author: mumta
 '''
 from flask import Flask
+import os
 
 from handlers import site
 from cem import cem
@@ -12,6 +13,7 @@ from batuhan import batuhan
 
 def create_app():
     app = Flask(__name__)
+    port = int(os.getenv("VCAP_APP_PORT"))
     app.config.from_object('settings')
     # blueprints
     app.register_blueprint(site)
@@ -23,7 +25,7 @@ def create_app():
 
 def main():
     app = create_app()
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
